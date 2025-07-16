b548942534298bc86a5c06240bc809b1cdca06b9
Jeff Griffiths <jeff@eko-recordings.ca>
2025-07-16 11:19:08 -0700

    should I even check this directory in

--- .svelte-kit/types/src/routes/signin/proxy+page.server.ts
+++
@@ -1,5 +0,0 @@
-// @ts-nocheck
-import { signIn } from '../../auth';
-import type { Actions } from './$types';
-export const actions = { default: signIn };
-;null as any as Actions;
--- .svelte-kit/types/src/routes/signout/proxy+page.server.ts
+++
@@ -1,5 +0,0 @@
-// @ts-nocheck
-import { signOut } from '../../auth';
-import type { Actions } from './$types';
-export const actions = { default: signOut };
-;null as any as Actions;
