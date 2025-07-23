0fa98fccbc99d282df0d3c39e5676586d8a70276
Jeff Griffiths <jeff@eko-recordings.ca>
2025-07-23 14:46:57 -0700

    dependencies

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,3 +7,20 @@
 	<li>Tailwind CSS 4</li>
 	<li>DaisyUI 5 for components</li>
 </ol>
+
+<h2>Getting started</h2>
+
+<p>I highly recommend using the Github command line utility `gh`:</p>
+
+<pre><code>
+	~ $ brew install gh
+</code></pre>
+
+<p>
+	Once you have `gh` installed, you can create a new project from this template repository usign
+	this command:
+</p>
+
+<pre><code>
+	~ $ gh repo create my-new-project --template jeffgca/sveltekit5-tw4-daisyui5
+</code></pre>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,3 @@
 <h1>About</h1>
 
-<p>This is a demo page for the Svelte 5 template.</p>
+<p>This is a demo about page for the Svelte 5 template.</p>
